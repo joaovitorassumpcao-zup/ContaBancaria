2c17d690c9c7f0f6aae16d4435a99275890af10c
João Vitor Cunha Assumpção <joao.assumpcao@zup.com.br>
2023-08-07 14:54:35 -0300

    ´ContaBancaria´ model

---
+++ src/main/java/com/catalisa/contabancaria/model/ContaBancaria.java
@@ -0,0 +1,42 @@
+package com.catalisa.contabancaria.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class ContaBancaria implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String numeroConta;
+
+    @Column(nullable = false)
+    private String agencia;
+
+    @Column(nullable = false)
+    private String nomeUsuario;
+
+    @Column(nullable = false)
+    private BigDecimal valorAtual;
+
+    @Column(nullable = false)
+    private BigDecimal valorFinal;
+
+    @Column(nullable = false)
+    private BigDecimal valorTransacao; // Valor para saque ou depósito
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TipoServico tipoServico; // Saque ou depósito
+
+}
---
+++ src/main/java/com/catalisa/contabancaria/model/TipoServico.java
@@ -0,0 +1,8 @@
+package com.catalisa.contabancaria.model;
+
+import java.io.Serializable;
+
+public enum TipoServico implements Serializable {
+    SAQUE,
+    DEPOSITO
+}
